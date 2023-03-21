96628edcdf54fd3d8be491c707ce35bada8ca901
nacho-esperanza <2112828@ucc.edu.ar>
2023-03-21 18:22:08 -0300

    prueba de errores en go

--- go-api/main.go
+++ go-api/main.go
@@ -4,11 +4,34 @@ import (
 	"encoding/json"
 	"fmt"
 	"go-api/items"
+	"errors"
 )
+
+func division(a int, b int) (int, error) {
+	if b == 0 {
+	return -1, errors.New("b no puede ser cero")
+	}
+	return a / b, nil
+	}
+
 
 func main() {
 
+
 	fmt.Println("Hello, world")
+
+	var a int = 10
+	var b int = 0
+
+	
+
+	div, err := division(a, b)
+	if err != nil {
+	fmt.Println("Error:", err.Error())
+	return
+	}
+	fmt.Println("Division:", div)
+
 
 	// Puedo acceder a la constante StoreName de package items porque está exportada (empieza con mayúscula)
 	fmt.Println(items.StoreName)
