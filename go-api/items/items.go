efd8d0b8ca0c6e4d87fddc82eed991e50740d746
RocioVulcano <rocio.vulcano.work@gmail.com>
2023-03-21 18:34:17 -0300

    Division

--- go-api/items/items.go
+++ go-api/items/items.go
@@ -1,23 +1,11 @@
 package items
 
-type Item struct {
-	Title       string
-	Description string
-	Price       float64
-	HasTaxes    bool
-}
+import "errors"
 
-// Constante exportada (empieza con mayúscula)
-const StoreName = "Tienda Oficial Adidas"
-
-// GetItem retorna un puntero a Item (*Item)
-func GetItem() *Item {
-
-	// Con & accedo a la dirección de memoria del Item, entonces equivale a un puntero
-	return &Item{
-		Title:       "Zapatillas Adidas",
-		Description: "Descripcion Zapatillas Adidas Talle 40",
-		Price:       30000,
-		HasTaxes:    false,
+func Division (a int,b int ) (int, error){
+	if b == 0 {
+		return -1, errors.New("b no puede ser cero")
 	}
+	return a / b, nil
 }
+
--- go-api/main.go
+++ go-api/main.go
@@ -1,26 +1,19 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"go-api/items"
 )
 
 func main() {
-
-	fmt.Println("Hello, world")
-
-	// Puedo acceder a la constante StoreName de package items porque está exportada (empieza con mayúscula)
-	fmt.Println(items.StoreName)
-
-	// LLamo a GetItem() del package items y obtengo un *Item que lo asigno en la variable item
-	item := items.GetItem()
-
-	// Convierto el item a un []byte en formato JSON
-	bytes, _ := json.MarshalIndent(item, "", "    ")
-
-	// Convierto el []byte a string para mostrarlo por terminal
-	jsonString := string(bytes)
-
-	fmt.Println(jsonString)
+	div, err := items.Division(8, 4)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("division: ", div)
+	}
+		
 }
+
+
+
