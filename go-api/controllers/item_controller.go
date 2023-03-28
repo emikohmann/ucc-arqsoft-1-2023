6b39fdfc5f77331d52e193635eb243daa37e6939
Emiliano Kohmann <emiliano.kohmann@spoton.com>
2023-03-28 18:07:24 -0300

    HTTP

---
+++ go-api/controllers/item_controller.go
@@ -0,0 +1,30 @@
+package controllers
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"go-api/services"
+	"net/http"
+	"strconv"
+)
+
+func GetItem(ctx *gin.Context) {
+
+	idString := ctx.Param("itemID")
+
+	id, err := strconv.ParseInt(idString, 10, 64)
+	if err != nil {
+		fmt.Println("Error parsing item ID", err)
+		ctx.JSON(http.StatusBadRequest, err)
+		return
+	}
+
+	item, err := services.GetItem(id)
+	if err != nil {
+		fmt.Println("Error getting item", err)
+		ctx.JSON(http.StatusInternalServerError, err)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, item)
+}
