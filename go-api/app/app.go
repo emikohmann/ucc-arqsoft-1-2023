912e26f604267dc99df7b4dc1ff834083b7ba642
Emiliano Kohmann <emiliano.kohmann@spoton.com>
2023-03-28 19:27:34 -0300

    Implementing GO API with HTTP

--- go-api/app/app.go
+++ go-api/app/app.go
@@ -6,11 +6,19 @@ import (
 	"go-api/controllers"
 )
 
+const (
+	port = ":8080"
+)
+
 func StartApp() {
 	router := gin.Default()
+
+	// Then we will move this to url_mapping in order to organize the code
 	router.GET("/items/:itemID", controllers.GetItem)
-	err := router.Run(":8080")
+
+	err := router.Run(port)
 	if err != nil {
+		// Then we will replace this error with a panic since it's a non-recoverable error
 		fmt.Println("Error running app", err)
 		return
 	}
--- go-api/services/item_service.go
+++ go-api/services/item_service.go
@@ -14,30 +14,41 @@ type ItemMercadoLibre struct {
 	Price float64 `json:"price"`
 }
 
-func GetItem(id int64) (domain.Item, error) {
-	endpoint := fmt.Sprintf("/items/MLA%d", id)
-	url := fmt.Sprintf("https://api.mercadolibre.com%s", endpoint)
+const (
+	mercadoLibreItemEndpoint = "/items/MLA%d"
+	mercadoLibreBaseURL      = "https://api.mercadolibre.com%s"
+)
 
+func GetItem(id int64) (domain.Item, error) {
+	// Build the URL
+	endpoint := fmt.Sprintf(mercadoLibreItemEndpoint, id)
+	url := fmt.Sprintf(mercadoLibreBaseURL, endpoint)
+
+	// Invoke MercadoLibre API
 	response, err := http.Get(url)
 	if err != nil {
 		return domain.Item{}, err
 	}
+
+	// Validate API Error
 	if response.StatusCode != http.StatusOK {
-		return domain.Item{}, errors.New(
-			fmt.Sprintf("unexpected status code %d", response.StatusCode))
+		return domain.Item{}, errors.New(fmt.Sprintf("unexpected status code %d", response.StatusCode))
 	}
 
+	// Read response payload bytes
 	bytes, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		return domain.Item{}, err
 	}
 
+	// Convert bytes to custom struct
 	var itemML ItemMercadoLibre
 	err = json.Unmarshal(bytes, &itemML)
 	if err != nil {
 		return domain.Item{}, err
 	}
 
+	// Map MercadoLibre item to Item
 	return domain.Item{
 		ID:    id,
 		Name:  itemML.Title,
