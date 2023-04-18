1679d07f5bd5b4ffc2bbc7868ffd3bd9e1969dbd
nacho-esperanza <2112828@ucc.edu.ar>
2023-04-18 15:45:22 -0300

    instalamos react y creamos carpeta frontend utilizando materialize

---
+++ frontend/client/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Item from './pages/item';
+
+function App() {
+  return (
+    <div className="App">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"></link>
+      <h1 class="blue-text text-darken-2">PlanasMiau</h1>
+
+      
+      <Item />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/client/src/pages/item.js
@@ -0,0 +1,14 @@
+
+function Item() {
+    return (
+      <div className="Item">
+        <h2>Titulo del Item Miau</h2>
+        <hr />
+        <h2>$ INFINITO</h2>
+        <hr />
+        <p>Descripcion del gatito.</p>
+      </div>
+    );
+  }
+  
+  export default Item;
