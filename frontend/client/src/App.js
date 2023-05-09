8de1babdd3ed3f8287a97915676101fe46aeb26b
leomorabito02 <leonardomorabito02@hotmail.com>
2023-05-09 17:27:23 -0300

    ahora siiiiiiiiiiiiii

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -2,42 +2,24 @@
 import './App.css';
 import Item from'./pages/items';
 
-import {BrowserRouter as Router, Routers, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"></link>
         <h1 className="card-panel light-blue lighten-1">Shop</h1>
-        <Item />
-
-        <body>
-
-        <form id="formulario_cliente"> 
-
-          <h5>Formulario Registro</h5>
-         <input type="text" name="Nombre" placeholder="Nombre"></input>
-         <input type="text" lastName="Apellido" placeholder="Apellido"></input>
-         <input type="email" email="Email" placeholder="dominio@email.com"></input>
-         <input type="text" name="usuario" placeholder="usuario_"></input>
-         <input type="password" name="pass" placeholder="Password"></input>
-         <p>Estoy de acuerdo con <a href="#">Terminos y Condiciones</a></p>
-         <button type="button"> Registrar </button>
-         <p><a href="#">¿Ya tengo Cuenta?</a></p>
-  
-      </form>
-    </body>
 
     <Router>
-      <Routers>
+      <Routes>
         <Route path = '/items/:id' element={<Item />}></Route>
-      </Routers>
+      </Routes>
     </Router>
 
     </div>
 
 
-    
+
 
 
   );
--- frontend/client/src/pages/items.js
+++ frontend/client/src/pages/items.js
@@ -1,23 +1,28 @@
-import React, {useEffect, useState} from "recat";
+import '../App.css';
 
+// todas las paginas que usan react tienen esto
+import React, {useEffect, useState} from "react";
+
+//nos permite adquirir todo lo que este asociado a las request
 import {useParams} from "react-router-dom";
 
 function Item() {
-  const [itemData, setItemData] = useState([{}]);
-  let (id) = useParams();
+    // State es como un objeto que va guardando informacion mientras los componentes sigan vivos
+    const [itemData, setItemData] = useState([{}]);
+    let {id} = useParams();
 
-  useEffect(() => {
-      setItemData(undefined);
-      fetch(`http://localhost:5001/api/items/$(id)`).then(
-        respo
-        .then(
-          data =>
+    useEffect(() => {
+        setItemData(undefined);
+        fetch(`http://localhost:5001/api/items/${id}`).then(
+            response => response.json()
+        ).then(
+            data => {
+                setItemData(data)
+            }
         )
-      )
-    } 
 
-  )
-
+        // los corchetes del final son una lista vacia que hace que se ejecute solamente el primer ... ?
+    }, []);
 
   return (
     <div className="Item">
